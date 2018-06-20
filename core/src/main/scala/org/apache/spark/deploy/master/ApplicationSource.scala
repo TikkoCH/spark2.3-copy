5e6bd150951ab8ed9e9ff4a215ab6cbbc06e38f2
krcys <772104764@qq.com>
2018-06-20 08:16:42 +0800

    metrics component

--- core/src/main/scala/org/apache/spark/deploy/master/ApplicationSource.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/ApplicationSource.scala
@@ -20,20 +20,24 @@ package org.apache.spark.deploy.master
 import com.codahale.metrics.{Gauge, MetricRegistry}
 
 import org.apache.spark.metrics.source.Source
-
+// scalastyle:off
+/**
+  * 集成应用程序的性能监测度量
+  * @param application
+  */
 private[master] class ApplicationSource(val application: ApplicationInfo) extends Source {
-  override val metricRegistry = new MetricRegistry()
+  override val metricRegistry = new MetricRegistry() //metrics的api
   override val sourceName = "%s.%s.%s".format("application", application.desc.name,
-    System.currentTimeMillis())
-
+    System.currentTimeMillis())//source的名称
+  //status 有这几个枚举状态:WAITING, RUNNING, FINISHED, FAILED, KILLED, UNKNOWN
   metricRegistry.register(MetricRegistry.name("status"), new Gauge[String] {
     override def getValue: String = application.state.toString
   })
-
+  //集成运行时间的监测
   metricRegistry.register(MetricRegistry.name("runtime_ms"), new Gauge[Long] {
     override def getValue: Long = application.duration
   })
-
+  //集成授权核心的监测
   metricRegistry.register(MetricRegistry.name("cores"), new Gauge[Int] {
     override def getValue: Int = application.coresGranted
   })
