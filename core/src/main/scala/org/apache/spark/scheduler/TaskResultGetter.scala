6084d893e9ee52a883164c995bc87702ccb4d13e
krcys <772104764@qq.com>
2018-07-12 18:03:29 +0800

    TaskResultGetter

--- core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
@@ -30,57 +30,79 @@ import org.apache.spark.serializer.SerializerInstance
 import org.apache.spark.util.{LongAccumulator, ThreadUtils, Utils}
 
 /**
- * Runs a thread pool that deserializes and remotely fetches (if necessary) task results.
+ * 运行一个线程池用于反序列化和远程获取task结果.
+  * Runs a thread pool that deserializes and remotely fetches (if necessary) task results.
  */
 private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedulerImpl)
   extends Logging {
-
+  /** 获取Task执行结果的线程数.可通过spark.resultGetter.threads属性配置,默认是4*/
   private val THREADS = sparkEnv.conf.getInt("spark.resultGetter.threads", 4)
 
-  // Exposed for testing.
+  // Exposed for testing.公开用于测试
+  /** 使用Executor的newFixedThreadPool方法创建ThreadPoolExecutor,用于提交获取Task执行结果的线程.
+    * 线程池由THREADS决定
+    * */
   protected val getTaskResultExecutor: ExecutorService =
     ThreadUtils.newDaemonFixedThreadPool(THREADS, "task-result-getter")
 
-  // Exposed for testing.
+  // Exposed for testing.公开用于测试
+  /**
+    * 通过使用本地线程缓存,保证在使用SerializerInstance时是线程安全的.
+    * */
   protected val serializer = new ThreadLocal[SerializerInstance] {
     override def initialValue(): SerializerInstance = {
       sparkEnv.closureSerializer.newInstance()
     }
   }
-
+  /**
+    * 都是线程变量,保证使用SerializerInstance对Task的执行结果进行反序列化是线程安全的.
+    * */
   protected val taskResultSerializer = new ThreadLocal[SerializerInstance] {
     override def initialValue(): SerializerInstance = {
       sparkEnv.serializer.newInstance()
     }
   }
-
+  /** up玫瑰处理执行成功的Task执行结果.*/
   def enqueueSuccessfulTask(
       taskSetManager: TaskSetManager,
       tid: Long,
       serializedData: ByteBuffer): Unit = {
     getTaskResultExecutor.execute(new Runnable {
       override def run(): Unit = Utils.logUncaughtExceptions {
-        try {
+        try { // 对Task的执行结果反序列化
           val (result, size) = serializer.get().deserialize[TaskResult[_]](serializedData) match {
             case directResult: DirectTaskResult[_] =>
+              // 如果Task结果类型为DirectTaskResult,说明Task的执行结果保存在DirectTaskResult
+
               if (!taskSetManager.canFetchMoreResults(serializedData.limit())) {
                 return
               }
+              // 不持锁反序列化value，以便它不会阻止其他线程。 我们应该在这里调用它，
+              // 这样当它在“TaskSetManager.handleSuccessfulTask”中再次调用时，它不需要反序列化该值。
               // deserialize "value" without holding any lock so that it won't block other threads.
               // We should call it here, so that when it's called again in
               // "TaskSetManager.handleSuccessfulTask", it does not need to deserialize the value.
+              // 只需对DirectTaskResult保存的数据进行反序列化
               directResult.value(taskResultSerializer.get())
+              // result,size元组
               (directResult, serializedData.limit())
             case IndirectTaskResult(blockId, size) =>
+              // 如果结果类型为IndirectTaskResult,说明Task结果没有保存在IndirectTaskResult中
               if (!taskSetManager.canFetchMoreResults(size)) {
                 // dropped by executor if size is larger than maxResultSize
+                // 如果尺寸过大,让executor删除
                 sparkEnv.blockManager.master.removeBlock(blockId)
                 return
               }
               logDebug("Fetching indirect task result for TID %s".format(tid))
+              // 需要向DAGSchedulerEventProcessLoop投递GettingResultEvent事件,
+              // 一直点就能看到发送GettingResultEvent事件.
               scheduler.handleTaskGettingResult(taskSetManager, tid)
+              // 从运行Task的节点上下载Block,
               val serializedTaskResult = sparkEnv.blockManager.getRemoteBytes(blockId)
               if (!serializedTaskResult.isDefined) {
+                // 如果在task结束之前和我们尝试获取结果之间时机器运行该任务失败,
+                // 或者如果blockManager必须将结果刷出,我们不可能获取task结果,
                 /* We won't be able to get the task result if the machine that ran the task failed
                  * between when the task ended and when we tried to fetch the result, or if the
                  * block manager had to flush the result. */
@@ -88,14 +110,18 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
                   taskSetManager, tid, TaskState.FINISHED, TaskResultLost)
                 return
               }
+              // 对获取到的结果反序列化
               val deserializedResult = serializer.get().deserialize[DirectTaskResult[_]](
                 serializedTaskResult.get.toByteBuffer)
               // force deserialization of referenced value
+              // 强制引用值的发序列化
               deserializedResult.value(taskResultSerializer.get())
+              // 删除block
               sparkEnv.blockManager.master.removeBlock(blockId)
               (deserializedResult, size)
           }
-
+          // 在从执行程序接收的累加器更新中设置任务结果大小。 我们需要在驱动程序上执行此操作，
+          // 因为如果我们在执行程序上执行此操作，那么在更新大小后我们将不得不再次序列化结果。
           // Set the task result size in the accumulator updates received from the executors.
           // We need to do this here on the driver because if we did this on the executors then
           // we would have to serialize the result again after updating the size.
--- core/src/main/scala/org/apache/spark/scheduler/local/LocalSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/local/LocalSchedulerBackend.scala
@@ -52,15 +52,21 @@ private[spark] class LocalEndpoint(
   extends ThreadSafeRpcEndpoint with Logging {
   // 空闲CPU内核数.提交Task运行之前,freeCores==totalCores
   private var freeCores = totalCores
-  /** local模式下,Driver处于同一JVM进程的Executor的身份标识,由于LocalEndpoint只在local模式下使用
-    * 
+  /** local模式下,Driver处于同一JVM进程的Executor的身份标识,由于LocalEndpoint只在local模式下使用,
+    * localExecutorId固定为driver
     * */
   val localExecutorId = SparkContext.DRIVER_IDENTIFIER
+  /**
+    * Driver处于同一JVM进程的Executor的host,固定为localhost
+    * */
   val localExecutorHostname = "localhost"
-
+  /**
+    * 与Driver处于同一JVM进程的Executor.由于LocalEndpoint的totalcores为1,所以本地只有一个Executor,
+    * 并且直接实例化.
+    * */
   private val executor = new Executor(
     localExecutorId, localExecutorHostname, SparkEnv.get, userClassPath, isLocal = true)
-
+  // 重写rpc中的receive方法
   override def receive: PartialFunction[Any, Unit] = {
     case ReviveOffers =>
       reviveOffers()
@@ -83,9 +89,13 @@ private[spark] class LocalEndpoint(
   }
 
   def reviveOffers() {
+    // 创建一个workerOffer
     val offers = IndexedSeq(new WorkerOffer(localExecutorId, localExecutorHostname, freeCores))
+    // resourceOffers给Task分配资源
     for (task <- scheduler.resourceOffers(offers).flatten) {
+      // freeCores-1
       freeCores -= scheduler.CPUS_PER_TASK
+      // 启动task
       executor.launchTask(executorBackend, task)
     }
   }
@@ -100,15 +110,20 @@ private[spark] class LocalEndpoint(
 private[spark] class LocalSchedulerBackend(
     conf: SparkConf,
     scheduler: TaskSchedulerImpl,
-    val totalCores: Int)
+    val totalCores: Int)    // CPU内核个数,固定为1
   extends SchedulerBackend with ExecutorBackend with Logging {
-
+  // 当前应用程序标识.
   private val appId = "local-" + System.currentTimeMillis
+  // LocalEndpoint的NettyRpcEndpoint
   private var localEndpoint: RpcEndpointRef = null
+  // 用户指定classpath
   private val userClassPath = getUserClasspath(conf)
+  // SparkContext中创建的LiveListenerBus
   private val listenerBus = scheduler.sc.listenerBus
+  // LauncherBackend匿名实现类
   private val launcherBackend = new LauncherBackend() {
     override def conf: SparkConf = LocalSchedulerBackend.this.conf
+    // 重写onStopRequest方法
     override def onStopRequest(): Unit = stop(SparkAppHandle.State.KILLED)
   }
 
@@ -121,26 +136,31 @@ private[spark] class LocalSchedulerBackend(
     val userClassPathStr = conf.getOption("spark.executor.extraClassPath")
     userClassPathStr.map(_.split(File.pathSeparator)).toSeq.flatten.map(new File(_).toURI.toURL)
   }
-
+  // 构造对象时进行连接
   launcherBackend.connect()
-
+  /** 启动LocalSchedulerBackend*/
   override def start() {
     val rpcEnv = SparkEnv.get.rpcEnv
+    // 创建LocalEndpoint,持有rpcEnv=NettyRpcEndpointRef
     val executorEndpoint = new LocalEndpoint(rpcEnv, userClassPath, scheduler, this, totalCores)
     localEndpoint = rpcEnv.setupEndpoint("LocalSchedulerBackendEndpoint", executorEndpoint)
+    // 事件总线中投递SparkListenerExecutorAdded消息
     listenerBus.post(SparkListenerExecutorAdded(
       System.currentTimeMillis,
       executorEndpoint.localExecutorId,
       new ExecutorInfo(executorEndpoint.localExecutorHostname, totalCores, Map.empty)))
+    // 向LauncherServer中发送appid
     launcherBackend.setAppId(appId)
+    // 向LauncherServer中stage
     launcherBackend.setState(SparkAppHandle.State.RUNNING)
   }
 
   override def stop() {
     stop(SparkAppHandle.State.FINISHED)
   }
-
+  /** 对Task进行资源分配后运行Task*/
   override def reviveOffers() {
+    // LocalEndpoint接收ReviveOffers消息后,会调用reviveOffers方法给下一个要调度的Task分配资源并运行Task
     localEndpoint.send(ReviveOffers)
   }
 
@@ -151,18 +171,23 @@ private[spark] class LocalSchedulerBackend(
       taskId: Long, executorId: String, interruptThread: Boolean, reason: String) {
     localEndpoint.send(KillTask(taskId, interruptThread, reason))
   }
-
+  /** Task状态更新.*/
   override def statusUpdate(taskId: Long, state: TaskState, serializedData: ByteBuffer) {
+    // 发送了一个StatusUpdate消息,localEndpoint接收到StatusUpdate消息后,会首先调用TaskScheduler的
+    // statusUpdate方法更新Task状态.
     localEndpoint.send(StatusUpdate(taskId, state, serializedData))
   }
 
   override def applicationId(): String = appId
-
+  /** 停止backend*/
   private def stop(finalState: SparkAppHandle.State): Unit = {
+    // 发送一个StopExecutor消息
     localEndpoint.ask(StopExecutor)
     try {
+      // 设置状态finalState
       launcherBackend.setState(finalState)
     } finally {
+      // 关闭
       launcherBackend.close()
     }
   }
